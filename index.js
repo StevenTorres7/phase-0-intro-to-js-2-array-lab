d5ec4574633af95526be2874ddffeacefea82ba2
Steven Torres <StevenTorres7@yahoo.com>
2022-10-23 18:43:18 -0400

    commit tree dde0597966be64b57d5db13fcc4d52761038a2bf

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat (name) {
+cats.push(name);
+return cats;
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name){
+  const ans = [...cats];
+    ans.push(name)
+    return ans;
+}
+
+function prependCat(name){
+    const ans = [...cats];
+        ans.unshift(name)
+        return ans;
+
+}
+
+function removeLastCat(name){
+    const ans = [...cats];
+        ans.pop(name);
+        return ans;
+}
+
+function removeFirstCat(name){
+    const ans = [...cats];
+        ans.shift(name);
+        return ans;
+}
